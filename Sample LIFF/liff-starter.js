5a4ff3f2248c3d8f4e88ad56d01569cf9bdb4711
unknown <prsl.yoanda@gmail.com>
2021-01-02 18:24:43 +0700

    first liff commit

--- Sample LIFF/liff-starter.js
+++ Sample LIFF/liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1655338120";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1655338129-4PR50l5d";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
